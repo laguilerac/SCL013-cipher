246cca8151bf437cb905582e457c17fa8671c44d
Loreto Aguilera <loreto.aguilerac@gmail.com>
2020-04-07 21:41:37 -0300

    cambié formula de encode y decode para que si, le ponemos más de 100 en offset, pueda cifrar y descifrar correctamente

--- src/cipher.js
+++ src/cipher.js
@@ -12,7 +12,7 @@ const cipher = {
         let mensajeDescifrado = "";
         for (let i=0; i<text.length; i++){
             let ascii= text.charCodeAt(i);
-            let posicion = ((ascii - 90 - parseInt(offset) + 26 * 2)% 26 + 90);
+            let posicion = ((ascii - 90 - parseInt(offset) - 26 * 2)% 26 + 90);
             mensajeDescifrado +=String.fromCharCode(posicion);
         }
         return mensajeDescifrado;

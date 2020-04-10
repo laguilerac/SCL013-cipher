2794f58fde24eca1f32cdd3aab52483e1a26e33e
Loreto Aguilera <loreto.aguilerac@gmail.com>
2020-04-10 19:29:38 -0300

    ŕeemplazado innerHTML por value para prevenir error en cipher.decode por funcón de borrar

--- src/index.js
+++ src/index.js
@@ -9,14 +9,15 @@ window.onload = function(){
         let message = document.getElementById("enterText").value.toUpperCase();
         let boxDecode = document.getElementById("showText");
         let mensajeCifrado = cipher.encode(nbr, message);
-        boxDecode.innerHTML = mensajeCifrado;
+        boxDecode.value = mensajeCifrado;
     });
     decodeButton.addEventListener("click", function(){
         let nbr = parseInt(document.getElementById("desplazar").value);
         let message = document.getElementById("enterText").value.toUpperCase();
         let boxDecode = document.getElementById("showText");
         let mensajeDescifrado = cipher.decode(nbr, message);
-        boxDecode.innerHTML = mensajeDescifrado;
+        console.log(mensajeDescifrado);
+        boxDecode.value = mensajeDescifrado;
     });
     cleanButton.addEventListener("click", function(){
         let message = document.getElementById("enterText");
